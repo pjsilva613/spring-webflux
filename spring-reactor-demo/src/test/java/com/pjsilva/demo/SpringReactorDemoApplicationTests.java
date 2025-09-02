1c5bba77e40bdefbf49ed4b2db21c8dd910713ee
pjsilva613 <pjsilva613@gmail.com>
2025-09-02 14:19:49 -0500

    proyecto base para aprender metodos basicos de webflux

---
+++ spring-reactor-demo/src/test/java/com/pjsilva/demo/SpringReactorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjsilva.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
